d82e6420eab0d46b4cf587f54f8e6a54364e3a56
goffinet <goffinet@goffinet.org>
2019-05-15 13:46:16 +0200

    update ubuntu template

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ docker run --rm                                     \
   -it                                               \
   --privileged                                      \
   --cap-add=ALL -v /lib/modules:/lib/modules        \
-  -v `pwd`:/opt/                                    \
+  -v `pwd`:/opt                                     \
   -v $HOME/.ssh/id_rsa:/root/.ssh/id_rsa            \
   -v $HOME/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub    \
   -w /opt/ goffinet/packer-qemu build ${template}
