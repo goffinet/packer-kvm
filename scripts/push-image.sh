0cd96af16f8c0489fe73407d6794d345821e9d1a
goffinet <goffinet@goffinet.org>
2020-09-10 19:16:08 +0200

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -16,8 +16,8 @@ cd ${path_image}
 mv packer-${image} ${image}.qcow2
 md5sum_image=$(md5sum ${image}.qcow2 | cut -d' ' -f1)
 size_image=$(stat -c %s ${image}.qcow2)
-md5sum ${image}.qcow2 > ${image}.qcow2.md5
-sha1sum ${image}.qcow2 > ${image}.qcow2.sha1
+md5sum ${image}.qcow2 > ${image}.qcow2.md5sum
+sha1sum ${image}.qcow2 > ${image}.qcow2.sha1sum
 
 # create a https://gns3.com appliance file
 
@@ -65,7 +65,46 @@ cat << EOF > ${image}.gns3a
 }
 EOF
 
-# Push the images by SCP
+# Push image to S3 bucket
 
-#scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ${image}.qcow2* root@$DESTINATION_SERVER:/var/www/html/kvm/
-#scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ${image}.gns3a root@$DESTINATION_SERVER:/var/www/html/gns3a/
+# Install python3-pip
+apt update && apt -y install python3-pip
+pip3 install awscli
+pip3 install awscli-plugin-endpoint
+# Configure parameters and credits
+mkdir ~/.aws
+cat << EOF > ~/.aws/config
+[plugins]
+endpoint = awscli_plugin_endpoint
+
+[default]
+region = fr-par
+s3 =
+  endpoint_url = https://s3.fr-par.scw.cloud
+  signature_version = s3v4
+  max_concurrent_requests = 100
+  max_queue_size = 1000
+  multipart_threshold = 50MB
+  # Edit the multipart_chunksize value according to the file sizes that you want to upload. The present configuration allows to upload files up to 10 GB (100 requests * 10MB). For example setting it to 5GB allows you to upload files up to 5TB.
+  multipart_chunksize = 10MB
+s3api =
+  endpoint_url = https://s3.fr-par.scw.cloud
+EOF
+cat << EOF > ~/.aws/credentials
+[default]
+aws_access_key_id=$AWS_ACCESS_KEY
+aws_secret_access_key=$AWS_SECRET_KEY
+EOF
+# Push the files
+aws s3 rm ${image}.qcow2 s3://$DESTINATION_SERVER/kvm/
+aws s3 rm ${image}.qcow2.md5sum s3://$DESTINATION_SERVER/kvm/
+aws s3 rm ${image}.qcow2.sha1sum s3://$DESTINATION_SERVER/kvm/
+aws s3 rm ${image}.gns3a s3://$DESTINATION_SERVER/gns3a/
+aws s3 cp ${image}.qcow2 s3://$DESTINATION_SERVER/kvm/
+aws s3api put-object-acl --bucket $DESTINATION_SERVER --key kvm/${image}.qcow2 --acl public-read
+aws s3 cp ${image}.qcow2.md5sum s3://$DESTINATION_SERVER/kvm/
+aws s3api put-object-acl --bucket $DESTINATION_SERVER --key kvm/${image}.qcow2.md5sum --acl public-read
+aws s3 cp ${image}.qcow2.sha1sum s3://$DESTINATION_SERVER/kvm/
+aws s3api put-object-acl --bucket $DESTINATION_SERVER --key kvm/${image}.qcow2.sha1sum --acl public-read
+aws s3 cp ${image}.gns3a s3://$DESTINATION_SERVER/gns3a/
+aws s3api put-object-acl --bucket $DESTINATION_SERVER --key gns3a/${image}.gns3a --acl public-read
