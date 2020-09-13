0e3ad71067dbb70de9a6a3c738ca67f7edc0c329
goffinet <goffinet@goffinet.org>
2020-09-13 14:00:09 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,17 @@
 
 template="$1"
 
+3_docker_docker-compose_installation () {
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+if [ -f /etc/debian_version ]; then
+apt-get update && apt-get -y install python3-pip
+elif [ -f /etc/redhat-release ]; then
+yum -y install python3-pip
+fi
+pip3 install docker-compose
+}
+
+which docker || 3_docker_docker-compose_installation
 docker run --rm \
   -e PACKER_LOG=1 \
   -e PACKER_LOG_PATH="packer-docker.log" \
