f109c236ac9ab4030f8f64934207baaec9dd5822
goffinet <goffinet@goffinet.org>
2021-06-14 10:02:21 +0200

    add terraform builder

---
+++ builder/README.md
@@ -0,0 +1,39 @@
+# How-to
+
+How-to create a on-time cloud server to build all the images of this reposirory and push them to a block storage.
+
+The example is based on the Scaleway provider and can publish 12 images in 35 minutes.
+
+```bash
+sudo apt update && sudo apt -y upgrade
+sudo apt -y install firewalld fail2ban git
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo -y apt install terraform
+sudo curl -o /usr/local/bin/scw -L "https://github.com/scaleway/scaleway-cli/releases/download/v2.3.1/scw-2.3.1-linux-x86_64"
+sudo chmod +x /usr/local/bin/scw
+echo export SCW_ACCESS_KEY="<SCALEWAY ACCESS KEY>" >> ~/.profile
+echo export SCW_SECRET_KEY="<SCALEWAY SECRET KEY>" >> ~/.profile
+echo export SCW_DEFAULT_ORGANIZATION_ID="<SCALEWAY ORGANIZATION ID>" >> ~/.profile
+echo export AWS_ACCESS_KEY=$SCW_ACCESS_KEY >> ~/.profile
+echo export AWS_SECRET_KEY=$SCW_SECRET_KEY >> ~/.profile
+sudo reboot
+```
+
+## Factory1
+
+
+```bash
+#!/bin/bash
+
+TIMESTAMP="$(date -I)-$(date +%s)"
+git clone https://github.com/goffinet/packer-kvm /opt/packer-kvm-$TIMESTAMP
+cd /opt/packer-kvm-$TIMESTAMP/builder
+terraform init
+mkdir -p /opt/log/packer
+source ~/.profile
+TF_LOG="TRACE" TF_LOG_PATH="/opt/log/packer/builder-apply-$TIMESTAMP.log" terraform apply -var="AWS_ACCESS_KEY=$AWS_ACCESS_KEY" -var="AWS_SECRET_KEY=$AWS_SECRET_KEY" -auto-approve
+TF_LOG="TRACE" TF_LOG_PATH="/opt/log/packer/builder-destroy-$TIMESTAMP.log" terraform destroy -var="AWS_ACCESS_KEY=$AWS_ACCESS_KEY" -var="AWS_SECRET_KEY=$AWS_SECRET_KEY" -auto-approve
+TF_LOG="TRACE" TF_LOG_PATH="/opt/log/packer/builder-destroy-$TIMESTAMP.log" terraform destroy -var="AWS_ACCESS_KEY=$AWS_ACCESS_KEY" -var="AWS_SECRET_KEY=$AWS_SECRET_KEY" -auto-approve
+
+```
---
+++ builder/builder.tf
@@ -0,0 +1,80 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source = "scaleway/scaleway"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "scaleway" {
+  zone            = "fr-par-1"
+  region          = "fr-par"
+}
+
+variable "AWS_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SECRET_KEY" {
+  type = string
+}
+
+locals {
+#  instance_type = "DEV1-L"
+#  instance_type = "GP1-XS"
+#  instance_type = "GP1-S"
+#  instance_type = "GP1-M"
+  instance_type = "GP1-L"
+#  instance_type = "GP1-XL"
+#  instance_type = "DEV1-S"
+#  instance_type = "DEV1-M"
+#  instance_type = "DEV1-XL"
+  tags = [ "builder" ]
+  count = 1
+}
+
+resource "scaleway_instance_ip" "public_ip" {
+count = local.count
+}
+
+resource "scaleway_instance_security_group" "builder" {
+  inbound_default_policy  = "accept"
+  outbound_default_policy = "accept"
+  name = "builder-${terraform.workspace}"
+}
+
+resource "scaleway_instance_server" "builder" {
+  count = local.count
+  name  = "builder-${count.index}"
+  type  = local.instance_type
+  image = "ubuntu-bionic"
+  tags = local.tags
+  enable_ipv6 = false
+  ip_id = scaleway_instance_ip.public_ip[count.index].id
+  security_group_id = scaleway_instance_security_group.builder.id
+#  provisioner "local-exec" {
+#    command = "ansible-playbook -i '${self.public_ip},' playbook.yml -e \"provider=scaleway\" ; ansible-playbook ../../playbooks/deploy.yml -e \"ansible_host=${self.public_ip}\""
+#  }
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt update && apt -y install git",
+      "git clone https://github.com/goffinet/packer-kvm",
+      "cd packer-kvm",
+      "echo export AWS_ACCESS_KEY=${var.AWS_ACCESS_KEY} >> ~/.profile",
+      "echo export AWS_SECRET_KEY=${var.AWS_SECRET_KEY} >> ~/.profile",
+      "source ~/.profile",
+      "curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh",
+      "for x in *.json ; do docker run --rm -e PACKER_LOG=1 -e PACKER_LOG_PATH=$x.log -d --name $x --privileged --cap-add=ALL -v /lib/modules:/lib/modules -v $PWD:/opt/ -e AWS_ACCESS_KEY=${var.AWS_ACCESS_KEY} -e AWS_SECRET_KEY=${var.AWS_SECRET_KEY} -w /opt/ goffinet/packer-qemu build $x ; done",
+      "while $(docker ps --format {{.Names}} | grep -q json) ; do echo waiting build ; sleep 60 ; done ; echo build finished ; true",
+    ]
+  }
+}
