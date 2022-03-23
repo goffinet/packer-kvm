1e932909e88b1ff662a3cc584ea8819a1280c059
goffinet <goffinet@goffinet.org>
2022-03-23 20:31:29 +0100

    fix centos8 stream and almalinux iso_checksum

--- build.sh
+++ build.sh
@@ -14,6 +14,13 @@ fi
 pip3 install docker-compose
 }
 
+if [ $template = "centos8.json" ] ; then
+iso_checksum=$(curl -s http://ftp.belnet.be/mirror/ftp.centos.org/8-stream/isos/x86_64/CHECKSUM | grep "SHA256 (CentOS-Stream-8-x86_64-latest-boot.iso) =" | sed 's/^.*= //g')
+packer_command="build -var \"iso_checksum=$iso_checksum\" centos8.json"
+else
+packer_command="build ${template}"
+fi
+
 which docker || docker_docker-compose_installation
 docker run --rm \
   --name ${template//.json/}-builder \
@@ -25,4 +32,4 @@ docker run --rm \
   -v `pwd`:/opt/ \
   -e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
   -e AWS_SECRET_KEY=$AWS_SECRET_KEY \
-  -w /opt/ goffinet/packer-qemu build ${template}
+  -w /opt/ goffinet/packer-qemu ${packer_command}
