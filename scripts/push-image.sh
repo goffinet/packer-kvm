1e6205766b2cbb2484cb8f04cbfd663beefd00df
goffinet <goffinet@goffinet.org>
2024-10-21 07:04:44 +0200

    sparsify images

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -24,9 +24,10 @@ if [ $disable == "true"  ] ; then exit ; fi
 
 cd ${path_image}
 
-# rename the image by sparsify, check the size, compute md5 sha256 sum
+# rename the image, check the size, compute md5 and sha1 sum
 
-virt-sparsify --check-tmpdir ignore --compress --convert qcow2 --format packer-${image} ${image}.qcow2
+mv packer-${image} ${image}.qcow2.temp
+virt-sparsify --check-tmpdir ignore --compress --convert qcow2 --format qcow2 ${image}.qcow2.temp ${image}.qcow2
 md5sum_image=$(md5sum ${image}.qcow2 | cut -d' ' -f1)
 size_image=$(stat -c %s ${image}.qcow2)
 md5sum ${image}.qcow2 > ${image}.qcow2.md5sum
